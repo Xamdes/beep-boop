a3f2dd8dfddbb6442a66ba9dbed5cb87a36c85de
Steven Colburn <stevencolburn@xamdes.com>
2018-06-08 11:25:45 -0700

    Added check for blank input

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,13 @@ $(function()
     var input = parseInt(inputString);
     var inputArray = inputString.split("");
     var output = "";
+    // Check for blank input
+    if(isNaN(input))
+    {
+      output = "";
+    }
     //If input is divisible by 3
-    if(!(input%3))
+    else if(!(input%3) && input !=0)
     {
       output += "I'm sorry, Dave. I'm afraid I can't do that.";
     }
