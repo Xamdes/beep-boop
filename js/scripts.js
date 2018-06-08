4b0f8f3b7b7a303b5bcd5de3538187b2d4c9ac8d
Steven Colburn <stevencolburn@xamdes.com>
2018-06-08 12:58:03 -0700

    add radio buttons to switch direction of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ $(function()
     event.preventDefault();
     var inputString = $("#input").val();
     var userName = $("#user-name").val().trim();
+    var direction = (parseInt($("input:radio[name=direction]:checked").val()));
     if(userName === "")
     {
       userName = "Dave";
     }
 
-    var output = processInput(inputString,userName);
+    var output = processInput(inputString,userName,direction);
     $("#output").html(output);
     $("#output-input").html(inputString);
   });
@@ -28,10 +29,9 @@ function contains(value,ptrArray)
   return returnValue;
 }
 
-function processInput(inputString,userName)
+function processInput(inputString,userName,direction)
 {
   var input = parseInt(inputString);
-  var inputArray = inputString.split("");
   var output = "";
   // Check for blank input
   if(isNaN(input))
@@ -42,40 +42,85 @@ function processInput(inputString,userName)
   {
     output = input.toString();
   }
-  else {
-    output = [];
-    for(var index = 0; index <= input; index++)
+  else
+  {
+    if(direction)
     {
-      var indexArray = index.toString().split("");
-      var temp = ("Input: " + index.toString()+" | Output: ");
-      //If input is divisible by 3
-      if(!(index%3) && index !=0)
-      {
-        temp+=("[\"I'm sorry, "+userName+". I'm afraid I can't do that.\"]");
-      }
-      //Number contains a 1
-      else if(contains("1",indexArray))
-      {
-        temp+=("[\"Boop!\"]");
-      }
-      //Number contains a 0
-      else if(contains("0",indexArray))
-      {
-        temp+=("[\"Beep!\"]");
-      }
-      //Output Number as is
-      else
-      {
-        temp+=index.toString();
-      }
-      output.push(wrapper("li",temp));
+      output = largeToSmallLoop(input,userName);
+    }
+    else
+    {
+      output = smallToLargeLoop(input,userName);
     }
   }
-
   return output;
 }
 
 function wrapper(what,input)
 {
   return ("<"+what+">"+input+"</"+what+">");
+}
+
+function smallToLargeLoop(input,userName)
+{
+  var output = [];
+  for(var index = 0; index <= input; index++)
+  {
+    var indexArray = index.toString().split("");
+    var temp = ("Input: " + index.toString()+" | Output: ");
+    //If input is divisible by 3
+    if(!(index%3) && index !=0)
+    {
+      temp+=("[\"I'm sorry, "+userName+". I'm afraid I can't do that.\"]");
+    }
+    //Number contains a 1
+    else if(contains("1",indexArray))
+    {
+      temp+=("[\"Boop!\"]");
+    }
+    //Number contains a 0
+    else if(contains("0",indexArray))
+    {
+      temp+=("[\"Beep!\"]");
+    }
+    //Output Number as is
+    else
+    {
+      temp+=index.toString();
+    }
+    output.push(wrapper("li",temp));
+  }
+  return output;
+}
+
+function largeToSmallLoop(input,userName)
+{
+  var output = [];
+  for(var index = input; index >= 0; index--)
+  {
+    var indexArray = index.toString().split("");
+    var temp = ("Input: " + index.toString()+" | Output: ");
+    //If input is divisible by 3
+    if(!(index%3) && index !=0)
+    {
+      temp+=("[\"I'm sorry, "+userName+". I'm afraid I can't do that.\"]");
+    }
+    //Number contains a 1
+    else if(contains("1",indexArray))
+    {
+      temp+=("[\"Boop!\"]");
+    }
+    //Number contains a 0
+    else if(contains("0",indexArray))
+    {
+      temp+=("[\"Beep!\"]");
+    }
+    //Output Number as is
+    else
+    {
+      temp+=index.toString();
+    }
+    output.push(wrapper("li",temp));
+  }
+  return output;
 }
